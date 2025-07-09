a6bb525b92df38e09e6092bcb15896153806b7cf
wsitcreator <wsitecreator2000@gmail.com>
2025-07-10 01:53:03 +0300

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -8,8 +8,19 @@ const path = require('path');
     args: ['--no-sandbox']
   });
   const page = await browser.newPage();
+
+  await page.setViewport({
+    width: 800,
+    height: 1200,
+    deviceScaleFactor: 2
+  });
+
   await page.goto('https://wsitcreator.github.io/stats-page/', {
     waitUntil: 'networkidle2'
+  });
+
+  await page.evaluate(() => {
+    document.body.style.backgroundColor = '#e7caa7';
   });
 
   await page.screenshot({ path: 'screenshot.png', fullPage: true });
